b67d0a261be82d74c1214da6adac7b637dc2d09c
chrismar303 <chrismar303@gmail.com>
2021-03-01 18:25:12 -0800

    Completed Basic Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,14 @@
+import backgroundImg from '../landing.jpg';
 
 const Home = () => {
-    return <h1>Home</h1>
+    return (
+        <main>
+            <img className="absolute object-cover w-full h-full " src={backgroundImg} alt="landing background"/>
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl font-bold text-red-400 leading-none lg:leading-snug home-name">I Am Chrismar</h1>
+            </section>
+        </main>
+    )
 }
 
 export default Home;
