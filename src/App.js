08ea3e50c617bacd897f21847097c3542df6ac07
chrismar303 <chrismar303@gmail.com>
2021-03-02 16:17:23 -0800

    Added Footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Post from './components/Post';
 import Project from './components/Project';
 import SinglePost from './components/SinglePost';
 import AppNavbar from './components/AppNavbar';
+import Footer from './components/Footer';
 import './App.scss';
 
 
@@ -19,6 +20,7 @@ function App() {
         <Route component={Post} path='/post' />
         <Route component={Project} path='/projects' />
       </Switch>
+      <Footer />
     </Router>
   );
 }
