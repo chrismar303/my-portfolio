7bdf25979b97163d7c086d453d9f8d69fda9794f
chrismar303 <chrismar303@gmail.com>
2021-03-03 17:09:16 -0800

    Improved Navbar UI

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -3,28 +3,34 @@ import { NavLink } from 'react-router-dom';
 
 const AppNavbar = () => {
     return (
-        <header className="text-red-600 body-font shadow-md">
-            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <header className="text-red-600 body-font ">
+            <nav className="border-b  container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
                 <NavLink to="/" exact className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-red-500 rounded-full" viewBox="0 0 24 24">
                     </svg>
                     <span className="ml-3 text-xl">CM</span>
                 </NavLink>
-                <nav className="font-medium text-lg md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-                    <NavLink to="/" exact className="mr-10 text-red-500 hover:text-red-800">Home</NavLink>
-                    <NavLink to="/about" className="mr-10 text-red-500 hover:text-red-800">About</NavLink>
-                    <NavLink to="/projects" className="mr-10 text-red-500 hover:text-red-800">Projects</NavLink>
-                    <NavLink to="/post" className="mr-10 text-red-500 hover:text-red-800">Post</NavLink>
-                </nav>
-                <nav>
+                <div className="font-medium text-lg flex flex-wrap items-center text-base justify-center">
+                    <Link text="Home" href="/" exact />
+                    <Link text="About" href="/about" />
+                    <Link text="Projects" href="/projects" />
+                    <Link text="Post" href="/post" />
                     <NavLink to="/contact" className="text-red-600">
                         <button className="font-medium inline-flex items-center bg-red-100 border-0 py-2 px-4 focus:outline-none hover:text-red-900 hover:bg-red-200 rounded text-base mt-4 md:mt-0">
                                 Contact me
                         </button>
                     </NavLink>
-                </nav>
-            </div>
+                </div>
+            </nav>
         </header>
+    )
+}
+
+function Link({ text, href, exact }) {
+    return (
+        <NavLink to={href} exact={exact || false} className="mr-10 px-2 py-2 text-red-500 hover:text-red-800 hover:bg-red-300 transition ease-in rounded-md">
+            {text}
+        </NavLink>
     )
 }
 
