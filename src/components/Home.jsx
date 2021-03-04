25c3e5d78a150a8b2e0b144c1527669ff35acbfe
chrismar303 <chrismar303@gmail.com>
2021-03-03 16:37:00 -0800

    Added Overlay to Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,8 @@ const Home = () => {
     return (
         <main>
             <img className="absolute object-cover w-full h-full " src={backgroundImg} alt="landing background"/>
-            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl font-bold text-red-400 leading-none lg:leading-snug home-name">I Am Chrismar</h1>
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8 bg-gray-800 bg-opacity-40">
+                <h1 className="text-6xl font-bold text-red-600 sticks leading-none lg:leading-snug home-name">I Am Chrismar</h1>
             </section>
         </main>
     )
