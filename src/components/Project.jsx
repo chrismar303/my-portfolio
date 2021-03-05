8c98e2bfab5b3bcf97259c2c946fea61b8fc2eaa
chrismar303 <chrismar303@gmail.com>
2021-03-05 15:06:09 -0800

    Changed Project card UI

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -19,7 +19,7 @@ const Project = () => {
     }, [projectsData]);
    
     return (
-        <main className="bg-gray-100 min-h-screen p-12">
+        <main className="bg-gray-100 min-h-screen px-2 md:px-4 py-12">
             <section className="container mx-auto">
                 {/* page title */}
                 <div className="text-center">
@@ -27,7 +27,7 @@ const Project = () => {
                     <h3 className="mt-4 text-gray-600">My latest works. Enjoy!</h3>
                 </div>
                 {/* Project Cards */}
-                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mt-4">
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-y-4 gap-x-4 mt-4">
                     {projectsData?.map((project, index) => <ProjectItem project={project} index={index} />)}
                 </div>
             </section>
@@ -38,42 +38,57 @@ const Project = () => {
 // TODO: add github links
 function ProjectItem({ project }) {
     return (
-        <article className="bg-white shadow-md rounded-md overflow-hidden">
-             {/* card header */}
-            <div className="pt-2 px-3 py-1">
-                <h1 className="lg:text-center text-2xl font-bold sticks">{project.title}</h1>
-            </div>
+        <article className="bg-white shadow-lg rounded-sm  overflow-hidden">
+            {/* Card img */}
             <img className="w-full h-40 bg-red-500"  />
+                    
             {/* card body */}
-            <div className="px-3 mt-2">
-                <div className="flex justify-between text-sm uppercase text-gray-400">
-                    <span>Date: </span>
-                    <span className="">{new Date(project.date).toLocaleDateString()}</span>
-                </div>
-               
-                <p className="text-md text-gray-700 leading-snug mt-2">{project.description}</p>
-                {/* links */}
-                <div className="flex my-4 text-center ">
-                    {project?.github && 
-                        <a href={project.github} className="flex-1 font-semibold text-green-500 border-green-500 border rounded-sm px-4 py-2 hover:bg-green-500 hover:text-white transition ease-in" alt="github" target="_blank" rel="noopener noreferrer">
-                            Github
-                        </a>
-                     }
-                    <a href={project.link} className="flex-1 font-semibold text-red-500 border-red-500 border rounded-sm px-4 py-2 hover:bg-red-500 hover:text-white transition ease-in" alt={project.title} target="_blank" rel="noopener noreferrer">
-                        Demo
-                    </a>
-                </div>
-            </div>
+            <ProjectBody project={project} />
+            
             {/* card footer */}
-            <div className="border-t border-gray-200 px-3 py-1 mt-2">
-                <h4 className="mb-1 text-sm text-gray-400">Tags:</h4>
-                <div className="flex text-xs gap-x-4 gap-y-2 py-1 overflow-x-auto">
-                    {project.tags.map(tag => (
-                        <span className="bg-red-100 text-red-600 px-4 py-0.5 font-semibold rounded-xl">{tag}</span>
-                    ))}
-                </div>
-            </div>
+            <ProjectFooter tags={project.tags} />
         </article >
+    )
+}
+
+function ProjectBody({ project }) {
+    return (
+        <div className="px-3 mt-2">
+            {/* metadata */}
+            <div className="flex justify-between text-xs uppercase text-gray-400">
+                <span>Date: </span>
+                <span className="">{new Date(project.date).toLocaleDateString()}</span>
+            </div>
+            {/* card header */}
+            <h1 className="lg:text-center text-xl font-bold sticks mt-2">{project.title}</h1>
+            
+            <p className="text-sm text-gray-700 leading-snug ">{project.description}</p>
+            {/* links */}
+            <div className="flex my-4 text-center ">
+                {project?.github && 
+                    <a href={project.github} className="flex-1 font-semibold text-green-500 border-green-500 border rounded-sm px-4 py-2 hover:bg-green-500 hover:text-white transition ease-in" alt="github" target="_blank" rel="noopener noreferrer">
+                        Github
+                    </a>
+                    }
+                <a href={project.link} className="flex-1 font-semibold text-red-500 border-red-500 border rounded-sm px-4 py-2 hover:bg-red-500 hover:text-white transition ease-in" alt={project.title} target="_blank" rel="noopener noreferrer">
+                    Demo
+                </a>
+            </div>
+        </div>
+    )
+}
+
+function ProjectFooter({ tags }) {
+    return (
+        <div className="border-t border-gray-200 px-3 py-1 mt-2">
+            <h4 className="mb-1 text-xs text-gray-400">Tags:</h4>
+            <div className="flex text-xs gap-x-4 gap-y-2 py-1 overflow-x-auto">
+                {tags.map(tag => (
+                    <span className="bg-red-100 text-red-600 px-4 py-0.5 font-semibold rounded-xl">{tag}</span>
+                ))}
+            </div>
+        </div>
+
     )
 }
 
