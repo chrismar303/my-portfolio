8779e299a3abbce84054ab432aaba4e59c7193ee
chrismar303 <chrismar303@gmail.com>
2021-03-09 20:38:14 -0800

    Increased text size of Navbar in mobile

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -21,11 +21,11 @@ const AppNavbar = () => {
                     </span>
                 </div>
                 {/* main */}
-                <div className={`${open ? 'left-0' : '-left-full'} duration-500 ease-in-out fixed bg-white  top-0 w-full h-full z-50 flex flex-col text-3xl  justify-center items-center lg:relative lg:block lg:left-0 lg:text-sm lg:w-max `}>
+                <div className={`${open ? 'left-0' : '-left-full'} duration-500 ease-in-out fixed bg-white  top-0 w-full h-full z-50 flex flex-col text-4xl  justify-center items-center lg:relative lg:block lg:left-0 lg:text-sm lg:w-max `}>
                     {/* close menu */}
                     <div 
                         onClick={_ => setOpen(false)}
-                        className="absolute top-5 right-3 text-2xl text-gray-500 lg:hidden"
+                        className="absolute top-3 right-3 text-4xl text-red-700 lg:hidden"
                     >
                         <MdClose />
                     </div>
@@ -44,7 +44,7 @@ const AppNavbar = () => {
 
 function Link({ text, href, exact }) {
     return (
-         <NavLink to={href} exact={exact || false} className="w-full py-8 px-6  text-center text-red-500  hover:text-white hover:bg-red-600 rounded-sm transition ease-in lg:px-4 lg:py-3">
+         <NavLink to={href} exact={exact || false} className="w-full py-6 px-6  text-center text-red-500  hover:text-white hover:bg-red-600 rounded-sm transition ease-in lg:px-4 lg:py-3">
             {text}
         </NavLink>
     )
@@ -52,55 +52,12 @@ function Link({ text, href, exact }) {
 
 function SocialLinks() {
     return (
-        <div className="absolute flex w-full bottom-4 justify-center gap-2 lg:hidden">
-            <SocialIcon url="https://www.linkedin.com/in/christian-martinez-b37868bb/" target="_blank" />
-            <SocialIcon url="https://github.com/chrismar303" target="_blank" />
-            <SocialIcon url="https://www.youtube.com/" target="_blank" />
+        <div className="absolute flex w-full bottom-10 justify-center gap-2 lg:hidden">
+            <SocialIcon fgColor="#fff" url="https://www.linkedin.com/in/christian-martinez-b37868bb/" target="_blank" />
+            <SocialIcon fgColor="#fff" url="https://github.com/chrismar303" target="_blank" />
+            <SocialIcon fgColor="#fff" url="https://www.youtube.com/" target="_blank" />
         </div>
     )
 }
-
-
-// const AppNavbar = () => {
-//     return (
-//         <header>
-//             <Navbar role="navigation" aria-label="main navigation">
-//                 <Navbar.Brand>
-//                     <Navbar.Item renderAs="a">
-//                         <NavLink to="/" exact>Chrismar</NavLink>
-//                     </Navbar.Item>
-//                     <Navbar.Burger />
-//                 </Navbar.Brand>
-//                 <Navbar.Menu>
-//                     <Navbar.Container position="end">
-//                         <Navbar.Item>
-//                             <NavLink to="/" exact>Home</NavLink>
-//                         </Navbar.Item>
-//                         <Navbar.Item>
-//                             <NavLink to="/about">About</NavLink>
-//                         </Navbar.Item>
-//                         <Navbar.Item>
-//                             <NavLink to="/projects">Projects</NavLink>
-//                         </Navbar.Item>
-//                         <Navbar.Item>
-//                             <NavLink to="/post">Post</NavLink>
-//                         </Navbar.Item>
-//                         <Navbar.Item>
-//                             <SocialIcon url="https://www.linkedin.com/in/christian-martinez-b37868bb/" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
-//                         </Navbar.Item>
-//                         <Navbar.Item>
-//                             <SocialIcon url="https://github.com/chrismar303" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
-//                         </Navbar.Item>
-//                         <Navbar.Item>
-//                             <SocialIcon url="https://www.youtube.com/" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }} />
-//                         </Navbar.Item>
-//                     </Navbar.Container>
-//                 </Navbar.Menu>
-
-//             </Navbar>
-                            
-//         </header>
-//     )
-// }
 
 export default AppNavbar;
