10019142f70ba81940d48d949afd9a734b18f863
chrismar303 <chrismar303@gmail.com>
2021-03-15 21:28:22 -0700

    Footer.jsx: Fixed Socials alignment on small to medium

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,20 +4,19 @@ import SocialLinks from './SocialLinks';
 const Footer = () => {
     return (
         <footer className="text-gray-600 body-font border-t border-gray">
-            <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col lg:w-10/12 ">
+            <div class="container px-5 py-8 mx-auto flex gap-6 items-center sm:flex-row flex-col lg:w-10/12 ">
                 <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
                     <span className="p-3  text-white rounded-full shadow bg-red-500">
                         { <FaTerminal />}
                     </span>
                     <span className="ml-3 text-xl">Chrismar</span>
                 </a>
-                <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+                <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-1">
                     <a href="" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">
                         © 2021 Chrismar
                     </a>
                 </p>
-                {/* TODO: Fix alignment on mediun */}
-                <div className="mt-5 text-lg lg:mt-0 lg:ml-auto">
+                <div className="text-lg lg:mt-0 lg:ml-auto">
                     <SocialLinks spacing={2} />
                 </div>
             </div>
