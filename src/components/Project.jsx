99670fb48cdd660a0da6c7dfafa526f50c3cb293
chrismar303 <chrismar303@gmail.com>
2021-03-16 00:24:20 -0700

    Project.jsx: Improved UI

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,28 +2,31 @@ import Section from './Section';
 import { projectsData } from '../data.js'
 
 const Project = () => {
-   
+// TODO: make card have max-size
     return (
         <main className="bg-gray-50  px-2 md:px-4">
             <Section title="My Projects" subtitle="My latest works. Enjoy!">
                 {/* Project Cards */}
-                <div className="grid md:grid-cols-2 lg:grid-cols-1 gap-y-4 gap-x-4 mt-4 lg:w-1/2 mx-auto">
-                    {projectsData?.map((project, index) => <ProjectItem project={project} index={index} />)}
+                <div className="grid md:grid-cols-2 lg:grid-cols-1 gap-y-4 gap-x-4 mt-4 md:w-3/4 lg:w-3/4 mx-auto">
+                    {projectsData?.map((project, index) => {
+                        const props = { project, index };
+                        return index % 2 === 0 ? <ProjectItem {...props} /> : <ProjectItem {...props} reverse />;
+                    })}
                 </div>
             </Section>
         </main>
     )
 }
 
-function ProjectItem({ project }) {
+function ProjectItem({ project, reverse }) {
     return (
-        <article className="bg-white shadow rounded-sm overflow-hidden  lg:rounded-md lg:flex">
+        <article className={`bg-white shadow rounded-sm overflow-hidden  lg:rounded-sm lg:shadow-md lg:flex ${reverse ? 'lg:flex-row-reverse' : ''}`}>
             {/* Card img */}
             <div className="lg:w-1/2 h-50 lg:h-auto bg-gray-100">
                 <img className="w-full h-full object-cover" src={project.image} />
             </div>
                     
-            <div className="lg:w-1/2">
+            <div className="lg:w-1/2 lg:flex lg:flex-col lg:justify-between">
                 {/* card body */}
                 <ProjectBody project={project} />
                 
@@ -47,7 +50,7 @@ function ProjectBody({ project }) {
             
             <p className="text-sm text-gray-700 leading-snug ">{project.description}</p>
             {/* links */}
-            <div className="flex my-4 text-center ">
+            <div className="flex items-center my-4 text-center ">
                 {project?.github && 
                     <a href={project.github} className="flex-1 font-semibold text-green-500 border-green-500 border rounded-sm px-4 py-2 hover:bg-green-500 hover:text-white transition ease-in" alt="github" target="_blank" rel="noopener noreferrer">
                         Github
@@ -63,7 +66,7 @@ function ProjectBody({ project }) {
 
 function ProjectFooter({ tags }) {
     return (
-        <div className="border-t border-gray-200 px-3 py-1 mt-2">
+        <div className="border-t border-gray-200 px-3 py-1 mt-2 bg-gray-50">
             <h4 className="mb-1 text-xs text-gray-400">Tags:</h4>
             <div className="flex text-xs gap-x-4 gap-y-2 py-1 overflow-x-auto">
                 {tags.map(tag => (
