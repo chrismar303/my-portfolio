107b9565c7e54bd0e74e45bcbb868131dca98d44
chrismar303 <chrismar303@gmail.com>
2021-11-27 18:07:53 -0800

    Commiting this months later
    This update is late. It should have been done a while back. I completed
    this in March. Now it is November.
    
    * This added a few pictures for the projects and added some details to
    them in data.js

--- src/data.js
+++ src/data.js
@@ -1,10 +1,15 @@
 import beachside from './assets/images/beachside.jpg';
 // import aboutImage from './assets/images/about2.jpg';
 // import aboutImage from './assets/images/about.JPG';
+import projectOne from './assets/images/project1.png';
+import projectThree from './assets/images/project3.jpg';
+import projectFour from './assets/images/project4.png';
+import projectFive from './assets/images/project5.jpg';
 import aboutImage from './assets/images/about3.JPG';
 import { FaReact, FaPython, FaJsSquare, FaJs, FaNodeJs, FaJava, FaHtml5, FaCss3Alt, FaGraduationCap } from "react-icons/fa";
 import { RiFilePaper2Line } from "react-icons/ri";
 
+//TODO : add project image overlay
 
 export const aboutData = {
     image: aboutImage,
@@ -29,30 +34,48 @@ export const aboutData = {
 
 export const projectsData = [
     { 
-        title: 'Chris', 
-        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, qui. Deserunt, incidunt non illo saepe odit sit, ipsa iure eius quo esse consequuntur velit ducimus atque provident neque dignissimos assumenda!', 
+        title: 'COVID-19 Tracker', 
+        description: 'React Application that tracks global data pertaining to COVID-19. Also allows for country-specific inspection of data.', 
+        github: 'https://github.com/chrismar303',
+        link: 'https://covid-19-tracker-e0dc2.firebaseapp.com/',
+        tags: ['React', 'Covid API', 'New York Times API'],
+        date: 'September 2020',
+        image: projectOne 
+    },
+    { 
+        title: 'FOODAR', 
+        description: 'A website that tracks a user’s list of establishments or restaurants the user would like to visit. When the logs in, establishments are listed in the order of closest proximity', 
         github: 'https://github.com/chrismar303',
         link: 'https://www.google.com',
-        tags: ['React', 'Python'],
-        date: new Date(),
+        tags: ['NodeJs', 'EJS', 'HTML', 'CSS3', 'Mongoose', 'PassportJS', 'YelpAPI'],
+        date: 'August 2020',
         image: beachside 
     },
     { 
-        title: 'Chris', 
-        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, qui. Deserunt, incidunt non illo saepe odit sit, ipsa iure eius quo esse consequuntur velit ducimus atque provident neque dignissimos assumenda!', 
+        title: 'Stock ROI Projector', 
+        description: 'A Python program that tracks the user’s Return On Investment based on the user’s selected time interval and growth expectations.', 
         github: 'https://github.com/chrismar303',
         link: 'https://www.google.com',
-        tags: ['React', 'Python'],
-        date: new Date(),
-        image: beachside 
+        tags: ['Python'],
+        date: 'April 2021',
+        image: projectThree 
     },
     { 
-        title: 'Chris', 
-        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus, qui. Deserunt, incidunt non illo saepe odit sit, ipsa iure eius quo esse consequuntur velit ducimus atque provident neque dignissimos assumenda!', 
+        title: 'Class Tracker', 
+        description: 'A python script that uses the selenium library to automate the process of checking a class’ availability status.', 
         github: 'https://github.com/chrismar303',
         link: 'https://www.google.com',
-        tags: ['React', 'Python', 'C', 'C++', 'Java', 'Javascript'],
-        date: new Date(),
-        image: beachside 
-    }
+        tags: ['Python', 'BeautifulSoup'],
+        date: 'July 2020',
+        image: projectFour 
+    },
+    { 
+        title: 'Movie Tickets Link Scraper', 
+        description: 'A python script that saves automates the process of finding all AMC movie showtimes based on a given zip code. Ticket purchase links and showtimes are saved onto a local file.', 
+        github: 'https://github.com/chrismar303',
+        link: 'https://www.google.com',
+        tags: ['Python', 'Selenium'],
+        date: 'September 2020',
+        image: projectFive 
+    },
 ]
