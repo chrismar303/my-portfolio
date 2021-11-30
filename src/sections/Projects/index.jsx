d977a7a6190cf7fabee0f435830441d37e9bbcf8
chrismar303 <chrismar303@gmail.com>
2021-11-30 01:49:02 -0800

    Changed 1 Project per row on medium devices

--- src/sections/Projects/index.jsx
+++ src/sections/Projects/index.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
         <main id="projects" className="bg-gray-50  px-2 md:px-4">
             <Section title="My Projects" subtitle="My latest works. Enjoy!">
                 {/* Project Cards */}
-                <div className="grid md:grid-cols-2 lg:grid-cols-1 gap-y-4 gap-x-4 md:w-3/4 lg:w-3/4 lg:gap-8 lg:py-4 mx-auto">
+                <div className="grid md:grid-cols-1 lg:grid-cols-1 gap-y-4 gap-x-4 md:gap-y-8 md:w-3/4 lg:w-3/4 lg:gap-8 lg:py-4 mx-auto">
                     {projectsData?.map((project, index) => {
                         const props = { project, index };
                         return index % 2 === 0 ? <Project {...props} /> : <Project {...props} reverse />;
