953a62e92f7249cdfdea9b1183825fdf64621ebe
chrismar303 <chrismar303@gmail.com>
2023-02-12 20:44:39 -0800

    [Feat] Added Mario Hat and Catch Phrase
    Added Mario Hat on top of my photo and updated the text below to say
    "It's A Me!"

--- src/sections/Home/HomeBanner.jsx
+++ src/sections/Home/HomeBanner.jsx
@@ -2,11 +2,12 @@ import meImg from '../../assets/images/me.jpg';
 
 const HomeBanner = () => {
     return (
-        <div className="flex flex-col items-center py-8 md:py-4 flex-1 h-full  justify-center relative w-full ">
-            <div className="rounded-full overflow-hidden">
+        <div className="flex flex-col items-center py-8 md:py-4 flex-1 h-full justify-center relative w-full ">
+            <div className="relative rounded-full overflow-hidden">
+                <img className='absolute lg:w-48 left-1/3 lg:-ml-8 lg:-mt-3 md:w-32 md:-mt-3 md:-ml-6 w-20 -ml-4 -mt-1.5 z-10' src="https://www.free-emoticons.com/files/funny-emoticons/7816.png" alt="Hat Mario Emoticon" />
                 <img className="h-40 md:h-64 lg:h-96 object-scale-down" src={meImg} alt="beach"/>
             </div>
-            <div><h3 className="text-red-500 font-bold text-2xl">me</h3></div>
+            <div><h3 className="text-red-500 font-bold text-2xl">It's A Me!</h3></div>
         </div>
     )
 }
