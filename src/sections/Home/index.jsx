e918137fb0d195be6aaba0f1be5e9ee0f1507d93
chrismar303 <chrismar303@gmail.com>
2023-02-15 17:41:40 -0800

    Fixed misaligned Banner

--- src/sections/Home/index.jsx
+++ src/sections/Home/index.jsx
@@ -3,7 +3,7 @@ import HomeBanner from './HomeBanner';
 const Home = () => {
     return (
         <main id="home" className="px-6 border-b-2 min-h-screen">
-            <section className="flex flex-col min-h-screen  py-20 lg:py-32 lg:flex-row">
+            <section className="flex flex-col items-center min-h-screen  py-20 lg:py-32 lg:flex-row">
                 {/* left / top */}
                 <HomeMain />
                 {/* right / bottom */}
