4aa596f8448533b7351c65bb0877b2b923f342e3
chrismar303 <chrismar303@gmail.com>
2023-02-15 17:51:37 -0800

    Center All Sections on High Res monitors

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,6 @@
 const Section = ({ title, subtitle, children }) => {
     return (
-        <section className="container mx-auto min-h-screen py-24 lg:py-32 lg:px-12">
+        <section className="flex flex-col justify-center items-center container mx-auto min-h-screen py-24 lg:py-32 lg:px-12">
             {/* header */}
             <Header title={title} subtitle={subtitle} />
             {children}
